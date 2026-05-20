464a8b67faf2a5b2fb168b74951d15055e4ffd3a
durgaprasaduniq <t.m.durgaprasad165@gmail.com>
2026-05-20 11:53:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mybucketviv1"
+    bucket = "devopsdurgajob"
     key    = "backend/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-south-2"
   }
 
   required_providers {
@@ -14,14 +14,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-2"
 }
 
 resource "aws_instance" "my_server" {
   instance_type        = "t2.micro"
   ami                  = "ami-0a1235697f4afa8a4"
   key_name             = "newmumbaikey"
-  availability_zone    = "ap-south-1b"
+  availability_zone    = "ap-south-2b"
   hibernation          = true
 
   root_block_device {
